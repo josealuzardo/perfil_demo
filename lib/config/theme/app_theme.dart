7161b1abe09948b2ab80142bbb5232b8e7d01137
Alejandro Luzardo <alejandro@strix.com.ve>
2025-07-29 10:14:28 -0400

    feat(theme): set color theme from mock

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -5,12 +5,12 @@ class AppTheme {
     useMaterial3: true,
     brightness: Brightness.dark,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Color(0xFFFFD600),
+      seedColor: Color.fromARGB(255, 255, 196, 0),
       brightness: Brightness.dark,
-      primary: Color(0xFFFFD600), // Amarillo vibrante
-      onPrimary: Colors.black,
-      secondary: Color(0xFFBDBDBD), // Gris claro
-      onSecondary: Colors.black,
+      primary: Color(0xFFFFB900), // Amarillo vibrante
+      onPrimary: Color(0xFF060606),
+      secondary: Color(0xFFFFB900), // Gris claro
+      onSecondary: Color(0xFF060606),
       surface: Color(0xFF1E1E1E),
       onSurface: Colors.white,
       error: Colors.red,
